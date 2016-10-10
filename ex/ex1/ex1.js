1556bd27c3590f8d452bece2f0428c39aaa00b1c
Awad Odeh <awadodeh@gmail.com>
2016-10-09 22:27:43 -0500

    delete unnecessery files

--- ex/ex1/ex1.js
+++
@@ -1,5 +0,0 @@
-require('http').createServer(function(req, res){
-     require('fs').createReadStream('./node.jpg').pipe(res);
-}).listen(8081, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:8081/');
