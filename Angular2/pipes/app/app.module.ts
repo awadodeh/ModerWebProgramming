3ff691fcc339c372c04126f060f4de2d8fc2ea45
Awad Odeh <awadodeh@gmail.com>
2016-10-22 15:31:48 -0500

    custom pipe

---
+++ Angular2/pipes/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductCalculatePricePipe } from './product-calculate-price.pipe';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    ProductDetailComponent,ProductCalculatePricePipe 
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
---
+++ Angular2/pipes/app/product-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { Product } from './product';
+
+@Component({
+  selector: 'my-product-detail',
+
+
+   template: `
+       <div *ngIf="product">
+      <h2>{{product.name}} details!</h2>
+      <div><label>id: </label>{{product.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="product.name" placeholder="name"/>
+      </div>
+
+      <div>quantity: <input [(ngModel)]="quantity"></div>
+      <div>
+        <label>total price: </label>
+        {{ product.price | calculatePrice : quantity }}
+      </div>
+    </div>
+  `
+})
+
+
+export class ProductDetailComponent {
+  
+  @Input()  
+  product:Product;
+  products: Product[];
+}
